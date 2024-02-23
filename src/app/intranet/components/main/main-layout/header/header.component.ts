082e380dbe492286d41fcd455f65b7144800de33
josselin.calderon <josselin.calderon@grupotawa.com>
2024-02-22 19:14:27 -0500

    estilos, header

--- src/app/intranet/components/main/main-layout/header/header.component.ts
+++ src/app/intranet/components/main/main-layout/header/header.component.ts
@@ -11,31 +11,56 @@ import { AuthService } from '../../../../services/auth.service';
 })
 export class HeaderComponent {
 
+  listMenu: any[]=[]
+
   permissionRequest: PermissionRequest = new PermissionRequest();
 
   constructor(
     private authService: AuthService
-  ){
+  ) {
+    const menu = localStorage.getItem('menu');
+    if (menu !== null) {
+      localStorage.removeItem('menu');
+    }
     this.getObjectPermissions();
   }
 
-  getObjectPermissions(){
+  getObjectPermissions() {
     const codempresa = localStorage.getItem('codempresa')
     const codpais = localStorage.getItem('codpais')
     const codnegocio = localStorage.getItem('codnegocio')
     const codcuenta = localStorage.getItem('codcuenta')
     const user = localStorage.getItem('user')
-    if (codempresa !== null && codpais!==null 
-      && codnegocio!==null && codcuenta!==null 
+    if (codempresa !== null && codpais !== null
+      && codnegocio !== null && codcuenta !== null
       && user !== null) {
       this.permissionRequest.codempresa = codempresa;
       this.permissionRequest.codpais = codpais;
       this.permissionRequest.codnegocio = codnegocio;
       this.permissionRequest.codcuenta = codcuenta;
       this.permissionRequest.user = user;
-      this.authService.getPermissions(this.permissionRequest).subscribe(res=>{
+      this.authService.getPermissions(this.permissionRequest).subscribe(res => {
         console.log(res);
+        localStorage.setItem('menu', JSON.stringify(res));
+        const menu = localStorage.getItem('menu');
+        if (menu !== null) {
+          console.log(JSON.parse(menu));
+          this.listMenu=JSON.parse(menu)
+        }
       })
     }
   }
+
+  // filtrar(data:any[]){
+  //   let modulosfiltro=[];
+  //   data.forEach(modulo=>{
+  //     const mods={
+  //       nombre:modulo.nombremodulo,
+  //       submodulos:modulo.submodules
+
+  //     }
+  //   })
+  // }
+
+
 }
