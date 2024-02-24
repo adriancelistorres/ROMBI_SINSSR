9d5e2c7aced74fff536c828e16d5c4261b0fb939
Adrian Celis Torres <adrian.celis@grupotawa.com>
2024-02-24 01:51:37 -0500

    cambios minimos

--- src/app/intranet/components/main/main.routes.ts
+++ src/app/intranet/components/main/main.routes.ts
@@ -3,23 +3,24 @@ import { MainLayoutComponent } from './main-layout/main-layout.component';
 import { MainStartComponent } from './main-layout/main-start/main-start.component';
 
 export const routes: Routes = [
-    {
-        path: '',
-        component: MainLayoutComponent,
+  {
+    path: '',
+    component: MainLayoutComponent,
         children: [
-            {
-                path: '',
-                pathMatch: 'full',
-                redirectTo: 'start'
-            },
-            {
-                path: 'start',
-                component: MainStartComponent
-            },
-            {
-                path: 'EntelRetail',
-                loadChildren: () => import('./../entel-retail/entel-retail.routes').then(m => m.routes)
-            },
-        ]
-    },
-]
+        {
+            path: '',
+            pathMatch: 'full',
+            redirectTo: 'start',
+        },
+        {
+            path: 'start',
+            component: MainStartComponent,
+        },
+        {
+            path: 'EntelRetail',
+            loadChildren: () =>
+            import('./../entel-retail/entel-retail.routes').then((m) => m.routes),
+        },
+        ],
+  },
+];
