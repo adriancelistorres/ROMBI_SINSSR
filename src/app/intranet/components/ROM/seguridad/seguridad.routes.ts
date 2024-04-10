26f001e30ff1861710a1a9c48ab789c2254794d2
Adrian Celis Torres <adrian.celis@grupotawa.com>
2024-04-10 10:43:40 -0500

    cambios generales2

--- src/app/intranet/components/ROM/seguridad/seguridad.routes.ts
+++ src/app/intranet/components/ROM/seguridad/seguridad.routes.ts
@@ -1,5 +1,4 @@
 import { Routes } from "@angular/router";
-import { SeguridadComponent } from "./seguridad.component";
 import { authGuard } from "../../../../core/guards/auth.guard";
 import { routesGuard } from "../../../../core/guards/routes.guard";
 import { NotFoundComponent } from "../../../shared/not-found/not-found.component";
